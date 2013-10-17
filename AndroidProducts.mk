64d9ce60c50210614e591235aadcbc7a13fd2cf2
David Hou <david.hou314@gmail.com>
2013-10-17 16:29:35 -0700

    Omnify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_d2att.mk \
-    $(LOCAL_DIR)/custom_d2att.mk
+    $(LOCAL_DIR)/omni_d2att.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 add_lunch_combo full_d2att-userdebug
-add_lunch_combo custom_d2att-userdebug
-add_lunch_combo custom_d2att-eng
+add_lunch_combo omni_d2att-userdebug
+add_lunch_combo omni_d2att-eng
